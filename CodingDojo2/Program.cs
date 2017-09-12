1b6cfcd8f8cf7b5bc744af7777838eb4ace7389e
Sascha Böck <wi16b142@technikum-wien.at>
2017-09-12 21:18:40 +0200

    create class stack with inner class node, added methods push, pop, peek to stack

--- CodingDojo2/Program.cs
+++ CodingDojo2/Program.cs
@@ -11,6 +11,44 @@ namespace CodingDojo2
         static void Main(string[] args)
         {
             Console.WriteLine("CodingDojo2 - let's get ready to rumble!");
+
+            //create obj stack with first node (value = 4)
+            Stack myStack = new Stack(4);
+        }
+
+        class Stack
+        {
+            class node
+            {
+                int number;
+                private node next = null;
+                internal node(int num)
+                {
+                    int number = num;
+                }
+            }            
+
+            private node first;
+
+            public Stack(int num)
+            {
+                this.first = new node(num);
+            }
+
+            public void push(int num)
+            {
+                Console.WriteLine("push {0} in stack", num);
+            }
+
+            public void pop()
+            {
+                Console.WriteLine("pop from stack");
+            }
+
+            public void peek()
+            {
+                Console.WriteLine("peek element from top of the stack");
+            }
         }
     }
 }
